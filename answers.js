2f4822c8fbd6751a53739749b1a13eda5613b0c8
Sonia <sonia.badeau@gmail.com>
2016-01-29 19:34:57 +0000

    answers.js

--- answers.js
+++ answers.js
@@ -28,10 +28,32 @@ var printPositives = numbers.forEach(function(number) {
 
 var newArray = [1,-90, 20, -30, -6];
 
-var getPositives = newArray.filter(function(number){
+var getPositives = newArray.filter(newFunction);  
+ function newFunction(number){
         if (number > 0){
-            console.log(number);
+           return number;
         }
-    })
-    
+    }
+    console.log(getPositives);
 
+//Exercise 3
+//Write a function called filterArray that takes an array AND a function as arguments. 
+//Your filter function should return a new array that contains only the elements where the passed function returns a truthy value.
+//NOTE: You are allowed to use Array.prototype.filter to answer this question.
+//NOTE 2: This is a bit of a trick question, the answer is a one-liner :)
+
+function filterArray(array, func){
+    return array.filter(func);
+}
+
+console.log(filterArray(newArray, newFunction));
+
+function addNumber(number){
+    return number + 1;
+}
+
+function addArray (array, func){
+    return array.filter(func);
+}
+
+console.log(addArray(newArray, addNumber));
