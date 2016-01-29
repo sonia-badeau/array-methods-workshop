546197490687c9fc5462a290a5829d8a2a72ee9c
Sonia <sonia.badeau@gmail.com>
2016-01-29 19:06:42 +0000

    answers.js

--- answers.js
+++ answers.js
@@ -11,4 +11,27 @@ function printPositives(number) {
 }
 printPositives();
 
+//other syntax
+
+var numbers = [1,-45, 20, 30, -6];
+var printPositives = numbers.forEach(function(number) {
+        if (number > 0){
+            console.log(number);
+        }
+});
+
+
+
+//Exercise 2
+//Similar to the previous exercise, write a function called getPositives that takes an array 
+//and uses its filter method to return a new array with only the positive numbers
+
+var newArray = [1,-90, 20, -30, -6];
+
+var getPositives = newArray.filter(function(number){
+        if (number > 0){
+            console.log(number);
+        }
+    })
+    
 
